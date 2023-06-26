b0da81c103a1171103ad32f7b9899fd5a4d0c801
Dayglow0926 <dayglow0926@gmail.com>
2023-06-26 11:21:23 +0900

    초기화

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col space-y-2  p-5 ">
-      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
-        Hello everyone!
-      </p>
-    </div>
-  );
-}
