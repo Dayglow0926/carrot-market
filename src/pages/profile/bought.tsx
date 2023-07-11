3e4fccd4818d6a0a9b2d5955d1d3b0c76d9c73cc
Dayglow0926 <dayglow0926@gmail.com>
2023-07-11 23:07:43 +0900

    feat: profile 페이지

---
+++ src/pages/profile/bought.tsx
@@ -0,0 +1,23 @@
+import Item from "@/components/item";
+import Layout from "@/components/layout";
+import { NextPage } from "next";
+
+const Bought: NextPage = () => {
+  return (
+    <Layout title="구매내역" canGoBack>
+      <div className="flex flex-col space-y-5 divide-y  pb-10">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            key={i}
+            id={i}
+            title="iPhone 14"
+            price={99}
+            hearts={1}
+            image=""
+          />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+export default Bought;
