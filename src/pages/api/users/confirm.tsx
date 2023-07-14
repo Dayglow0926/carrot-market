6a2163e298fc7fde2cd7c6b9a1a774879d47b8bb
Dayglow0926 <dayglow0926@gmail.com>
2023-07-14 17:27:22 +0900

    add: 유저 확인용 confirm api 추가

---
+++ src/pages/api/users/confirm.tsx
@@ -0,0 +1,39 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+): Promise<any> {
+  console.log(req.session);
+  const { token } = req.body;
+
+  const foundToken = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+
+  if (!foundToken) return res.status(404).end();
+
+  req.session.user = {
+    id: foundToken.userId,
+  };
+
+  await req.session.save();
+
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+
+  res.status(200).end();
+
+  res.json({ ok: true });
+}
+
+export default withApiSession(withHandler("POST", handler));
