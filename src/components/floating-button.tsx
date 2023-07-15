41e5db346f50b0683478635df464d56eaa0ebadc
Dayglow0926 <dayglow0926@gmail.com>
2023-07-15 21:20:49 +0900

    fix: Link 하위 a 태그 삭제

--- src/components/floating-button.tsx
+++ src/components/floating-button.tsx
@@ -7,10 +7,11 @@ interface FloatingButton {
 
 export default function FloatingButton({ children, href }: FloatingButton) {
   return (
-    <Link href={href}>
-      <a className="fixed bottom-24 right-5 flex aspect-square w-14 cursor-pointer  items-center justify-center rounded-full border-0 border-transparent bg-orange-400 text-white shadow-xl transition-colors hover:bg-orange-500">
-        {children}
-      </a>
+    <Link
+      className="fixed bottom-24 right-5 flex aspect-square w-14 cursor-pointer  items-center justify-center rounded-full border-0 border-transparent bg-orange-400 text-white shadow-xl transition-colors hover:bg-orange-500"
+      href={href}
+    >
+      {children}
     </Link>
   );
 }
--- src/components/item.tsx
+++ src/components/item.tsx
@@ -11,41 +11,43 @@ interface ItemProps {
 
 export default function Item({ title, price, hearts, id, image }: ItemProps) {
   return (
-    <Link href={`/products/${id}`}>
-      <a className="flex cursor-pointer justify-between px-4 pt-5">
-        <div className="flex space-x-4">
-          <div className="relative h-20 w-20  rounded-md">
-            <Image
-              className="fill-inherit object-cover"
-              src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${image}/public`}
-              alt={image}
-            />
-          </div>
-          <div className="flex flex-col pt-2">
-            <h3 className="text-sm font-medium text-gray-900">{title}</h3>
-            <span className="mt-1 font-medium text-gray-900">${price}</span>
-          </div>
+    <Link
+      className="flex cursor-pointer justify-between px-4 pt-5"
+      href={`/products/${id}`}
+    >
+      <div className="flex space-x-4">
+        <div className="relative h-20 w-20  rounded-md">
+          <Image
+            className="fill-inherit object-cover"
+            src={image}
+            // src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${image}/public`}
+            alt={image}
+          />
         </div>
-        <div className="flex items-end justify-end space-x-2">
-          <div className="flex items-center space-x-0.5 text-sm  text-gray-600">
-            <svg
-              className="h-4 w-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              ></path>
-            </svg>
-            <span>{hearts}</span>
-          </div>
+        <div className="flex flex-col pt-2">
+          <h3 className="text-sm font-medium text-gray-900">{title}</h3>
+          <span className="mt-1 font-medium text-gray-900">${price}</span>
         </div>
-      </a>
+      </div>
+      <div className="flex items-end justify-end space-x-2">
+        <div className="flex items-center space-x-0.5 text-sm  text-gray-600">
+          <svg
+            className="h-4 w-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            ></path>
+          </svg>
+          <span>{hearts}</span>
+        </div>
+      </div>
     </Link>
   );
 }
